939b5b08d34ece506efd1a13a97a20f31d5a5ac3
Bram Van Mensel <Bram.VanMensel@digipolis.be>
2019-02-28 02:25:34 +0100

    heroku http port assign

--- src/serverScript.js
+++ src/serverScript.js
@@ -6,8 +6,8 @@ import GameServer from './server/GameServer'
 import httpServer from './server/httpServer'
 
 global.window = global.document = global;
-httpServer.listen(port);
-console.log('http server running at http://127.0.0.1:' + port + '/');
+httpServer.listen(process.env.PORT||port);
+console.log('http server running at http://127.0.0.1:' + (process.env.PORT||port) + '/');
 
 var webSocketServer = new WebSocketServer({
   httpServer: httpServer
