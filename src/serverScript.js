5c19072646d3719d63b7bf3023487f472b170634
Bram Van Mensel <bram.vanmensel@nxtport.eu>
2020-05-14 14:56:26 +0200

    update defualt port

--- src/serverScript.js
+++ src/serverScript.js
@@ -1,13 +1,13 @@
 
 var WebSocketServer = require('websocket').server;
-var port = 3000;
+var port = 8080;
 
 import GameServer from './server/GameServer'
 import httpServer from './server/httpServer'
 
 global.window = global.document = global;
 httpServer.listen(process.env.PORT || port);
-console.log('http server running at ' + (process.env.WEBSOCKET_URL || 'http://127.0.0.1') + ':' + (process.env.PORT || 3000) + '/');
+console.log('http server running at ' + (process.env.WEBSOCKET_URL || 'http://127.0.0.1') + ':' + (process.env.PORT || port) );
 
 var webSocketServer = new WebSocketServer({
   httpServer: httpServer
